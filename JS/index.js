5e5199de9b47011b3e8cfafbd9ab6ea0ba9b16cb
vladislav <forest8537@gmail.com>
2021-05-04 18:36:42 +1000

    first commit

---
+++ JS/index.js
@@ -0,0 +1,36 @@
+[].slice.call(document.querySelectorAll('.dropdown .nav-link')).forEach(function(el){
+    el.addEventListener('click', onClick, false);
+});
+
+function onClick(e){
+    e.preventDefault();
+    var el = this.parentNode;
+    el.classList.contains('show-submenu') ? hideSubMenu(el) : showSubMenu(el);
+}
+
+function showSubMenu(el){
+    el.classList.add('show-submenu');
+    document.addEventListener('click', function onDocClick(e){
+        e.preventDefault();
+        if(el.contains(e.target)){
+            return;
+        }
+        document.removeEventListener('click', onDocClick);
+        hideSubMenu(el);
+    });
+}
+
+function hideSubMenu(el){
+    el.classList.remove('show-submenu');
+}
+
+// "use strict";
+
+// let c = 45;
+
+// function draw() {
+//   document.documentElement.style.setProperty('--direction', c++ + 'deg');
+//   requestAnimationFrame(draw);
+// }
+
+// requestAnimationFrame(draw);
